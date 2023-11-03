83f82457ac48e2d955f0df07ced49b8b9b4092ef
Timoté LANCELLE <timote.lancelle@outlook.fr>
2023-11-03 09:21:08 +0100

    CSS footer, page erreur

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,16 @@
 import Error from "../../components/Error/Error";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import "./ErrorPage.scss";
 
 function ErrorPage() {
     return (
-        <div>
-            <Error />
+        <div className="errorPage">
+            <Header />
+            <div className="errorPage_mid">
+                <Footer />
+                <Error />
+            </div>
         </div>
     );
 }
